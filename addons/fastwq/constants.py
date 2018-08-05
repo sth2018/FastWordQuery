801fa19be9dd1256e3a62cee4af6b4f317407caa
St.Huang <wenhonghuang@gmail.com>
2018-08-05 21:02:06 +0800

    version number

--- addons/fastwq/constants.py
+++ addons/fastwq/constants.py
@@ -23,7 +23,7 @@ from .lang import _
 
 __all__ = ['VERSION', 'Endpoint', 'Template']
 
-VERSION = 'v1.2.2'
+VERSION = 'v1.2.3'
 
 class Endpoint:
     repository = u'https://github.com/sth2018/FastWordQuery'
